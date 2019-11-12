1e935c1f89cea51dd9d0a4d174f89dc14ed012c2
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-11-12 14:21:21 -0800

    Updates

---
+++ precache-manifest.fe669301f88d6cea264bf76d8696270c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af222dc64603e744ae9c59e151747c43",
+    "url": "/HackerNewsAPI/index.html"
+  },
+  {
+    "revision": "e38a733511459e764d6c",
+    "url": "/HackerNewsAPI/static/css/main.7ecce42f.chunk.css"
+  },
+  {
+    "revision": "949fdc043aa5809396df",
+    "url": "/HackerNewsAPI/static/js/2.2427daae.chunk.js"
+  },
+  {
+    "revision": "e38a733511459e764d6c",
+    "url": "/HackerNewsAPI/static/js/main.d4dcf75b.chunk.js"
+  },
+  {
+    "revision": "af4aed08c0b6de1d97cc",
+    "url": "/HackerNewsAPI/static/js/runtime~main.91d8dd33.js"
+  }
+]);
